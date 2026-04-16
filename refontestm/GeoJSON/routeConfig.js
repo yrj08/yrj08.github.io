ed218f2adbfedb50fb502d96c2a96c88002d727a
yrj08 <qdyangruijia@gmail.com>
2026-04-16 15:40:37 -0400

    Update routeConfig.js

--- refontestm/GeoJSON/routeConfig.js
+++ refontestm/GeoJSON/routeConfig.js
@@ -3019,6 +3019,26 @@ window.routeConfigs = {
       <li>Améliorer la vitesse commerciale.</li>
     `},
 
+  "475": {title: "Ligne 475 • Express Dollard-des-Ormeaux",
+    files: {currentShapes: "../GeoJSON/curr-475.geojson", newShapes: "../GeoJSON/475.geojson", 
+            currentStops: "../GeoJSON/oldstops.geojson", newStops: "../GeoJSON/newstops.geojson"},
+    directionLabels: {outbound: "Est – Station Du Collège", 
+                      inbound: "Ouest – Cégep Gérald-Godin"},
+    branchStyles: {
+      "4758888": {line: {color: "#009EE0", weight: 4, opacity: 1},
+          stop: {radius: 4, weight: 3, color: "#009EE0", fillOpacity: 1, shape: "circle"}},
+      "4759999": {line: {color: "#5DBDEA", weight: 4, opacity: 1},
+          stop: {radius: 4, weight: 3, color: "#5DBDEA", fillOpacity: 1, shape: "circle"}},
+      "current": {line: {color: "#FFB350", weight: 10, opacity: 1},
+          stop: {radius: 5, weight: 5, color: "#FFB350", fillOpacity: 1, shape: "circle"}}},
+    changements: `
+      <li></li>
+      <li>Retrait de quelques arrêts trop rapprochés.</li>
+    `,
+    pourquoi: `
+      <li>Améliorer la vitesse commerciale.</li>
+    `},
+  
   "491": {title: "Ligne 491 • Express Provost",
     files: {currentShapes: "../GeoJSON/curr-491.geojson", newShapes: "../GeoJSON/491.geojson", 
             currentStops: "../GeoJSON/oldstops.geojson", newStops: "../GeoJSON/newstops.geojson"},

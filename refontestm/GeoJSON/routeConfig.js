d78c4d726de2204157aa06ce67b86beabc8b0bcc
yrj08 <qdyangruijia@gmail.com>
2026-04-20 10:40:50 -0400

    Update routeConfig.js

--- refontestm/GeoJSON/routeConfig.js
+++ refontestm/GeoJSON/routeConfig.js
@@ -939,6 +939,26 @@ window.routeConfigs = {
       <li>Améliorer la vitesse commerciale.</li>
     `},
 
+  "85": {title: "Ligne 85 • Hochelaga",
+    files: {currentShapes: "../GeoJSON/curr-085.geojson", newShapes: "../GeoJSON/085.geojson", 
+            currentStops: "../GeoJSON/oldstops.geojson", newStops: "../GeoJSON/newstops.geojson"},
+    directionLabels: {outbound: "Est – des Sciences / des Bâtisseurs", 
+                      inbound: "Ouest – Station Frontenac"},
+    branchStyles: {
+      "858888": {line: {color: "#009EE0", weight: 4, opacity: 1},
+          stop: {radius: 4, weight: 3, color: "#009EE0", fillOpacity: 1, shape: "circle"}},
+      "859999": {line: {color: "#5DBDEA", weight: 4, opacity: 1},
+          stop: {radius: 4, weight: 3, color: "#5DBDEA", fillOpacity: 1, shape: "circle"}},
+      "current": {line: {color: "#FFB350", weight: 10, opacity: 1},
+          stop: {radius: 5, weight: 5, color: "#FFB350", fillOpacity: 1, shape: "circle"}}},
+    changements: `
+      <li></li>
+      <li>Retrait de quelques arrêts trop rapprochés.</li>
+    `,
+    pourquoi: `
+      <li>Améliorer la vitesse commerciale.</li>
+    `},
+  
   "90": {title: "Ligne 90 • Saint-Jacques",
     files: {currentShapes: "../GeoJSON/curr-090.geojson", newShapes: "../GeoJSON/090.geojson", 
             currentStops: "../GeoJSON/oldstops.geojson", newStops: "../GeoJSON/newstops.geojson"},

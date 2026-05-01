940719ffc1e9d744d233822323e9a2cf97c2e908
yrj08 <qdyangruijia@gmail.com>
2026-05-01 11:28:53 -0400

    Update displayHelpers.js

--- refontestm/GeoJSON/displayHelpers.js
+++ refontestm/GeoJSON/displayHelpers.js
@@ -1,6 +1,6 @@
 // displayHelpers.js
 
-function formatStopName(stopProps, imagePath = "images/") {
+function formatStopName(stopProps, imagePath = "../GeoJSON/images/") {
   const stopName = stopProps.stop_name || "";
 
   const icons =
